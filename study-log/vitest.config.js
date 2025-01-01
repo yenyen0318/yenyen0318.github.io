437768675009789a6928fbc893a3fe93de8d5f4b
yenyen0318 <yen.work.0318@gmail.com>
2025-01-01 17:18:29 +0800

    feat: add deployment script and base URL for GitHub Pages

--- study-log/vitest.config.js
+++ study-log/vitest.config.js
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: '/yenyen0318.github.io/',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
