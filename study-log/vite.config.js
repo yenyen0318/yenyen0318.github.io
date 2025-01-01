47d82532d8dced3a5eec9e57fbeb34442e3dcdc5
yenyen0318 <yen.work.0318@gmail.com>
2025-01-01 17:23:48 +0800

    feat: update base URL for GitHub Pages in Vite configuration

--- study-log/vite.config.js
+++ study-log/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/yenyen0318.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
--- study-log/vitest.config.js
+++ study-log/vitest.config.js
@@ -5,7 +5,6 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    base: '/yenyen0318.github.io/',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
